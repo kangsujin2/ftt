d115ed2a40af0127a799a24d96633f9b52492dbd
kangsujin2 <ksujin367@gmail.com>
2023-08-12 17:12:27 +0900

    Initial commit

---
+++ src/test/java/com/my/ftt/FttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.ftt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
